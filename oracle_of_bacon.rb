b8bfdc42427d5feb0c5cb0b29a45cb2668da5d3e
Patrickmnts <patrickmnts@gmail.com>
2013-06-12 15:48:07 -0700

    working URI

--- oracle_of_bacon.rb
+++ oracle_of_bacon.rb
@@ -1,17 +1,20 @@
 require 'open-uri'
-require 'uri'
-require 'net/http'
+require 'cgi'
+# require 'uri'
+# require 'net/http'
 
 def bacon
-  from = ('GeorgeClooney')
-  to = ('KevinBacon')
+  from = 'George Clooney'
+  from_escape = CGI::escape(from)
+  to = 'Kevin Bacon'
+  to_escape = CGI::escape(to)
   #options are pre-determined by API call
   option = (1)
   # 1 includes movies, 3 includes movies and tv
 
-  uri = URI.parse("http://oracleofbacon.org/cgi-bin/xml?a=#{from}&b=#{to}&u=#{option}&p=google-apps")
-  response = Net::HTTP.get(uri)
-  #response = open("http://oracleofbacon.org/cgi-bin/xml?a=#{from}&b=#{to}&u=#{option}&p=#{location}").read
+  #uri = URI.parse("http://oracleofbacon.org/cgi-bin/xml?a=#{from}&b=#{to}&u=#{option}&p=google-apps")
+  #response = Net::HTTP.get(uri)
+  response = open("http://oracleofbacon.org/cgi-bin/xml?a=#{from_escape}&b=#{to_escape}&u=#{option}&p=google-apps").read
 
   puts response
 
