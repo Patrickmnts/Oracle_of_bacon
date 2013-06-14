91f6442558c06bff7e377ec7e8df044c17c5e21d
Neil Northrop <nnorthrop@gmail.com>
2013-06-13 17:00:19 -0700

    added to the api.rb

--- api.rb
+++ api.rb
@@ -1,5 +1,6 @@
 # Handles API call
-require 'rexml/document'
+require 'pp'
+require 'nokogiri'
 require 'open-uri'
 require 'cgi'
 
@@ -15,8 +16,31 @@ class API
     return open("http://oracleofbacon.org/cgi-bin/xml?a=#{from}&b=#{to}&u=#{option}&p=google-apps").read
   end
 
+# This method checks if you spelled the actors name right
+  def spellcheck?(response)
+    return
+  end
+
 # This parses our XML
-  def self.parse
-   # parsed_response = REXML::Document.new(response)
+  def self.parse(response)
+    parsed_response = Nokogiri::XML(response)
+    # The next two lines get specific elements from the XML document
+    actors = parsed_response.xpath("//actor")
+    movies = parsed_response.xpath("//movie")
+    # The following three variables are used in the response loop
+    movieIndex = 0
+    response = ""
+    firstPerson = true
+    # This loop builds the final response
+    actors.each do |actor|
+      response = response + actor
+      if (movies[movieIndex])
+        movie = movies[movieIndex].text
+        response = response + (firstPerson ? "" : " who") + " was in #{movie} with "
+      end
+      movieIndex += 1
+      firstPerson = false
+    end
+    return response
   end
 end
